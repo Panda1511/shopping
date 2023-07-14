e4aeb35095ae7a695e881b90ad0770f22c0816d3
Tran Quoc <trankienquoc151102@gmail.com>
2023-07-14 22:27:16 +0700

    first commit

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: "cluster0.pdq3xz4.mongodb.net",
+  DB_USER: "trankienquoc151102",
+  DB_PASS: "151102",
+  DB_DATABASE: "shoppingonline",
+  EMAIL_USER: "trankienquoc151102@gmail.com",
+  EMAIL_PASS: "ygfakkoxkditzedk",
+  JWT_SECRET: "<jwt_secret>",
+  JWT_EXPIRES: "86400000", // in milliseconds
+};
+module.exports = MyConstants;
